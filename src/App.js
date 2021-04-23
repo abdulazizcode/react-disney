83cf83c6f373ed885ce84a526e373f181b4be590
abdulazizcode <tajikcoder@gmail.com>
2021-04-23 14:14:38 +0800

    [INT] - header compleated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import React from "react";
 import "./index.css";
 import Login from "./components/Login";
-import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 export default function App() {
